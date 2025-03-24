919ff8b9a2cce9fb1a85eff049eca3402d970582
Optimus3264 <bhushanpatil096@gmail.com>
2025-03-25 01:11:38 +0530

    Add files via upload

---
+++ Comparison.py
@@ -0,0 +1,23 @@
+a=10
+b=5
+
+print('a=',a)
+print('b=',b)
+
+#a is equal to b (==)
+print('a==b:', a==b) #False
+
+#a is less than b (<)
+print('a<b:',a<b) #False
+
+#a is greater than b (>)
+print('a>b:', a>b) #True
+
+#a is not equal to b (!=)
+print('a!=b:',a!=b) #True
+
+#a is less than or equal to b (<=)
+print('a<=b', a<=b)
+
+#a is greater than or equal to b (>=)
+print('a>=b', a>=b)
