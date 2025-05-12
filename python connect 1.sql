64e3574f3dd6fa195c56d6a329c8afa4c4c2a339
Optimus3264 <bhushanpatil096@gmail.com>
2025-05-12 22:48:20 +0530

    Add files via upload

---
+++ python connect 1.sql
@@ -0,0 +1,16 @@
+-- create database python_connect 1
+create database python_connect1;
+use python_connect1;
+
+select * from users;
+
+drop table users;
+
+select * from users;
+
+cursor.execute("INSERT INTO users(name , email) " values(%s,%s),
+( "John Doe ", "johndoe@example.com")),
+
+commit the changes conn.commit(),
+print("user inserted successfull!");
+
