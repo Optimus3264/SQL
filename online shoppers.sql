07bcd58041c3f29f48bcb38fb8900a4441d1081b
Optimus3264 <bhushanpatil096@gmail.com>
2026-06-06 22:40:32 +0530

    online shoppers.sql

--- online shoppers.sql
+++ online shoppers.sql
@@ -10,7 +10,7 @@ select * from online_shoppers_intention where month='Feb';
 -- how many entries in dataset where visitor type is new visitor?.
 select count(*) from online_shoppers_intention where visitortype='new_visitor'; -- for how many/how much use query 'count'
 
--- how many entries in dataset where visitor type is return_visitor?
+-- how many entries in dataset where visitor type is return_visitor?.
 select count(*) from online_shoppers_intention where visitortype='returning_visitor';
 
 -- how many entries of mon feb in dataset 
